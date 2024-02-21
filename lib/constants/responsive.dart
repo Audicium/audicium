385c1d7495e8ac066d5b565e9efeb8c8dc07d13c
RA341 <razerishi1505@gmail.com>
2024-02-20 19:31:56 -0500

    removed everything

--- lib/constants/responsive.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const mobileMaxWidth = 450.0;
-
-bool isMobile(BuildContext context) =>
-    MediaQuery.of(context).size.width < mobileMaxWidth;
--- lib/services/init.dart
+++
@@ -1,9 +0,0 @@
-import 'package:audicium/constants/utils.dart';
-import 'package:audicium/services/isar_service.dart';
-import 'package:audicium/services/library_service.dart';
-
-Future<void> initServices() async {
-  getIt
-    ..registerSingleton<IsarService>(await IsarService().init())
-    ..registerSingleton<LibraryService>(LibraryService());
-}
